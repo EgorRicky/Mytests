467c1fc75b50cb3034b35ca15ec3c7b31c0699bc
Egor Shein <e.shein@xsolla.com>
2022-10-20 23:18:20 +0500

    added yml

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ export const config = {
         "password": "Egor.123*:"
     },
     userId: "8d3c1a45-6f43-403e-9ac5-6d4f8c789779",
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkVnb3IxMjMiLCJwYXNzd29yZCI6IkVnb3IuMTIzKjoiLCJpYXQiOjE2NjIzNTg3MDF9.TGWxrS0eGwv76Br-dtAj9P1l-GUkdiMLQAFCxdXcgto'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkVnb3IxMjMiLCJwYXNzd29yZCI6IkVnb3IuMTIzKjoiLCJpYXQiOjE2NjYyODc3OTd9.hVwDbyWyWbK1k7viwbfEJOchcjw1mTIJqcKDB3-BDm8'
 }
 export default config;
