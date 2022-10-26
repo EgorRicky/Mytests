a8bf743d17093b6357fb6436e188f53eff5fd27d
EgorRicky <85022581+EgorRicky@users.noreply.github.com>
2022-10-26 15:11:02 +0500

    rm

--- afterEach/afterEach.ts
+++ afterEach/afterEach.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-import config from '../config';
 
 export const deleteLauncher = (launcherId: string) =>
-  axios.delete(`${config.apiURL}/launcher_project/${launcherId}`, {
+  axios.delete(`${config.apiURL}/${launcherId}`, {
     headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${config.token}`,
+      Accept: 'application/json'
     },
   });
